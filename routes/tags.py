99309ff8cf9d7d04660526e3707b7303cd35898c
Haza Shami <hazashami@gmail.com>
2024-01-29 18:25:03 -0800

    bugfix when find_delta pops from the end of the list

--- routes/tags.py
+++ routes/tags.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from .route_util import is_authorized, unauthorized_response, _build_cors_preflight_response, _make_cors_response, get_db
+from .route_util import is_authorized, unauthorized_response, _build_cors_preflight_response, _make_cors_response, get_db, close_db
 from models.tag import Tag
 
 tags_api = Blueprint('tags', __name__, url_prefix = '/api/tags')
@@ -7,6 +7,23 @@ tags_api = Blueprint('tags', __name__, url_prefix = '/api/tags')
 #GET /tags/for/review/<review id>
 #queryparams: review id (required)
 #gets list of tags associated with a given review id
+@tags_api.route("/for/review/<review_id>", methods=['GET', 'OPTIONS'])
+def get_tags_for_review(review_id):
+    if request.method == 'OPTIONS':
+        return _build_cors_preflight_response(request.origin)
+    
+    res = _make_cors_response(request.origin)
+
+    connection = get_db()
+    cursor = connection.cursor()
+    query = Tag.get_tags_from_review_query(review_id)
+    cursor.execute(query)
+    result = cursor.fetchall()
+    close_db()
+
+    res.status = 200
+    res.set_data(result)
+    return res
 
 #GET /tags/list/reviews/<tag id>
 #queryparams: tag id (required)
@@ -39,12 +56,14 @@ def post_tags_for_review(review_id):
     
     connection = get_db()
     cursor = connection.cursor()
+
     (add_tags, delete_tags) = find_delta(review_id, tags, cursor)
     if len(add_tags) > 0:
         commit_tags_to_db(add_tags, review_id, connection, cursor)
     if len(delete_tags) > 0:
         remove_tags_from_db(delete_tags, review_id, connection, cursor)
-    connection.close()
+
+    close_db()
 
     res.status = 200
     return res
@@ -62,7 +81,11 @@ def find_delta(review_id, new_tags, cursor):
         if (db_tags[i]['c_tag_name'] == new_tags[j]):
             db_tags.pop(i)
             new_tags.pop(j)
-        if (j <= len(new_tags)):
+            if i > 0:
+                i = i - 1
+            if j > 0:
+                j = j - 1
+        elif (j == len(new_tags) - 1):
             i = i + 1
             j = 0
         else:
@@ -91,6 +114,7 @@ def commit_tags_to_db(tags, review_id, connection, cursor):
         add_tag_to_review(tag['c_id'], review_id, cursor)
     connection.commit()
 
+#checks for straggler tags and deletes them
 def remove_tags_from_db(tags, review_id, connection, cursor):
     for tag in tags:
         tag_id = tag['c_id']
@@ -104,6 +128,7 @@ def remove_tags_from_db(tags, review_id, connection, cursor):
             delete_tag(tag_id, cursor)
             connection.commit()
 
+#fetches tag_id for a given tag_name from the tag table
 def get_tag_id(tag_name, cursor):
     query = Tag.get_tag_query(tag_name)
     cursor.execute(query)
