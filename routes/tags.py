9d422a87a3ea6b26db03a677c3c0f494ce1b6702
Haza Shami <hazashami@gmail.com>
2024-02-06 14:39:17 -0800

    re-add find_delta function - FE would have to reorganize data significantly to differentiate and it may as well just render data

--- routes/tags.py
+++ routes/tags.py
@@ -63,48 +63,88 @@ def post_tags_for_review(review_id):
         return res
     
     tags = request.get_json()
-    if tags['review_id'] is None or tags['deleted'] is None or tags['added'] is None:
+    if len(tags) == 0:
         res.status = 400
         res.set_data("No tags to process")
         return res
     
     connection = get_db()
+    cursor = connection.cursor()
 
-    if len(tags['added']) > 0:
-        commit_tags_to_db(tags['added'], review_id, connection)
-    if len(tags['deleted']) > 0:
-        remove_tags_from_db(tags['deleted'], review_id, connection)
+    (add_tags, delete_tags) = find_delta(review_id, tags, cursor)
+    if len(add_tags) > 0:
+        commit_tags_to_db(add_tags, review_id, connection, cursor)
+    if len(delete_tags) > 0:
+        remove_tags_from_db(delete_tags, review_id, connection, cursor)
 
     close_db()
     
     res.status = 200
     return res
 
+#helper method to diff a given review's current tags against update tags.
+def find_delta(review_id, new_tags, cursor):
+    query = Tag.get_tags_from_review_query(review_id)
+    cursor.execute(query)
+
+    db_tags = cursor.fetchall()
+
+    i = 0
+    j = 0
+    while (i < len(db_tags)):
+        if (db_tags[i]['c_tag_name'] == new_tags[j]):
+            db_tags.pop(i)
+            new_tags.pop(j)
+            if i > 0:
+                i = i - 1
+            if j > 0:
+                j = j - 1
+        elif (j == len(new_tags) - 1):
+            i = i + 1
+            j = 0
+        else:
+            j = j + 1
+
+    return (new_tags, db_tags)
+
+#helper method to process tags to add to a review. prevents overlapping.
 def commit_tags_to_db(tags, review_id, connection):
+    new_tag_ids = []
+    existing_tag_ids = []
     cursor = connection.cursor()
+
     for tag in tags:
-        id = get_tag_id(tag['c_tag_name'], cursor) if tag['c_id'] is None else tag['c_id']
-        #confirmed as a brand new tag
+        id = get_tag_id(tag, cursor)
         if id is None:
-            add_tag(tag, cursor)
-            connection.commit()
-            id = get_tag_id(tag['c_tag_name'])
-        add_tag_to_review(id, review_id, cursor)
+            new_tag_ids.append(tag)
+        else:
+            existing_tag_ids.append({'c_id': id['c_id'], 'c_tag_name': tag})
+
+    for tag in new_tag_ids:
+        add_tag(tag, cursor)
+        connection.commit()
+        tag_id = get_tag_id(tag, cursor)
+        add_tag_to_review(tag_id['c_id'], review_id, cursor)
     connection.commit()
 
+    for tag in existing_tag_ids:
+        add_tag_to_review(tag['c_id'], review_id, cursor)
+    connection.commit()
+
+#checks for straggler tags and deletes them
 def remove_tags_from_db(tags, review_id, connection):
     cursor = connection.cursor()
     for tag in tags:
-        id = tag['c_id']
-        delete_tag_from_review(id, review_id, cursor)
+        tag_id = tag['c_id']
+        delete_tag_from_review(tag_id, review_id, cursor)
         connection.commit()
 
-        query = Tag.check_last_tag_query(id)
+        query = Tag.check_last_tag_query(tag_id)
         cursor.execute(query)
         result = cursor.fetchone()
         if result['COUNT(*)'] == 0:
-            delete_tag(id, cursor)
-    connection.commit()
+            delete_tag(tag_id, cursor)
+            connection.commit()
 
 #fetches tag_id for a given tag_name from the tag table
 def get_tag_id(tag_name, cursor):
